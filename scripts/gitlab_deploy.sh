bb57a980c0c9ff43ab0608a7fabb1705d213844a
Guillaume Beraudo <guillaume.beraudo@camptocamp.com>
2019-12-04 11:51:12 +0100

    Deploy to envs

---
+++ scripts/gitlab_deploy.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+DEV_BRANCH="master"
+PROD_BRANCH="prod"
+DEPLOY_TO_S3="${DEPLOY_TO_S3:-scripts/deploy_to_s3.sh}"
+
+echo tag $CI_COMMIT_TAG
+echo ref $CI_COMMIT_REF_NAME
+echo mr $CI_MERGE_REQUEST_SOURCE_BRANCH_NAME
+
+if [ -z "$CI_COMMIT_TAG$CI_COMMIT_REF_NAME" ]
+then
+  echo "I don't understand what to do. Skipping"
+  exit 1
+fi
+
+# the "int_*"" tags are deployed to the int environment
+# all other tags are ignored
+if [ -n "$CI_COMMIT_TAG" ]
+then
+  NAME="$CI_COMMIT_TAG"
+  echo "Deploying tag $NAME"
+  if [[ $NAME == int_* ]]
+  then
+    $DEPLOY_TO_S3 int
+    exit $?
+  fi
+
+  echo "Not an int tag, skipping"
+  exit 0
+fi
+
+# merge requests are deploayed to a review directory on the dev environment
+if [ -n "$CI_MERGE_REQUEST_SOURCE_BRANCH_NAME" ]
+then
+  NAME="$CI_MERGE_REQUEST_SOURCE_BRANCH_NAME"
+  echo "Deploying merge request $NAME"
+  if [[ $NAME == *_GSNGM-* ]]
+  then
+    $DEPLOY_TO_S3 review $NAME
+    exit $?
+  fi
+
+  echo "Not a recognized PR name, skipping"
+fi
+
+# the DEV_BRANCH branch is deployed to the dev environment
+# the PROD_BRANCH branch is deployed to the production environment
+# all other branches are ignored
+if [ -n "$CI_COMMIT_REF_NAME" ]
+then
+  NAME="$CI_COMMIT_REF_NAME"
+  echo "Deploying branch $NAME"
+  if [ "$NAME" = "$DEV_BRANCH" ]
+  then
+    $DEPLOY_TO_S3 dev
+    exit $?
+  fi
+  if [ "$NAME" = "$PROD_BRANCH" ]
+  then
+    $DEPLOY_TO_S3 prod
+    exit $?
+  fi
+
+  echo "Not a managed branch, skipping"
+  exit 0
+fi
+
+echo "If you see this there is something wrong"
+exit 1
---
+++ tests/test_gitlab_deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+function tester() {
+  tag="$1"
+  branch="$2"
+  mr="$3"
+  expected="$4"
+  echo -n "   tag='$tag' branch='$branch' mr='$mr' expected='$expected'        "
+  OUTPUT=`DEPLOY_SCRIPT="echo" \
+    CI_COMMIT_TAG="$tag" \
+    CI_COMMIT_REF_NAME="$branch" \
+    CI_MERGE_REQUEST_SOURCE_BRANCH_NAME="$mr" \
+    scripts/gitlab_deploy.sh`
+
+  if echo $OUTPUT | grep -q "$expected"
+  then
+    echo -e "\e[32mOK\e[0m"
+  else
+    echo -e "\e[31mKO\e[0m    "$OUTPUT
+    exit 1
+  fi
+}
+
+
+echo "Testing deploy job is working as expected"
+tester int "" "" "Not an int tag" # should skip deploying int tag
+tester int_something "" "" "deploy_to_s3.sh int" # should deploy int_* tags
+
+tester "" "master" "" "deploy_to_s3.sh dev" # should deploy the master branch
+tester "" "prod" "" "deploy_to_s3.sh prod" # should deploy the prod branch
+
+tester "" int int "skipping" # should skip deploying int branch (merge request)
+tester "" int blabla_GSNGM-45 "deploy_to_s3.sh review blabla_GSNGM-45" # should skip deploying int branch (merge request)
