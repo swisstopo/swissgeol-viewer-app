45c2870da2900bfa34d0787cdd6fd64716f06cff
Guillaume Beraudo <guillaume.beraudo@camptocamp.com>
2019-12-05 13:28:45 +0100

    Introduce deploy stage

--- scripts/deploy_to_s3.sh
+++ scripts/deploy_to_s3.sh
@@ -1,3 +1,51 @@
-#!/bin/sh
+#!/bin/bash
+
+
+DEV_BUCKET="ngmpub_dev_bdgi_ch"
+INT_BUCKET="ngmpub_int_bdgi_ch"
+PROD_BUCKET="ngmpub_prod_bdgi_ch"
+SEND_TO_S3="${SEND_TO_S3:-aws s3 sync}"
+
+function send_to_s3() {
+  BUCKET="$1"
+  PATH="$2"
+  aws s3 sync --delete --exclude prs dist/ s3://$BUCKET/$PATH
+
+}
 
 echo Called $0 $*
+
+ENV="$1"
+
+if [ "$ENV" = "prod"]
+then
+    send_to_s3 $PROD_BUCKET
+    exit $?
+fi
+
+if [ "$ENV" = "int"]
+then
+    send_to_s3 $INT_BUCKET
+    exit $?
+fi
+
+if [ "$ENV" = "dev"]
+then
+    send_to_s3 $DEV_BUCKET
+    exit $?
+fi
+
+if [ "$ENV" = "review"]
+then
+    BRANCH="$1"
+    if [ -z "$BRANCH"]
+    then
+      echo "Missing branch name for review env"
+      exit 1
+    fi
+    send_to_s3 $DEV_BUCKET /prs/$BRANCH
+    exit $?
+fi
+
+echo "Unknown env $ENV"
+exit 1
