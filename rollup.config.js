13b0c1e7305d0814f37d791ad8895835e870006f
Guillaume Beraudo <guillaume.beraudo@camptocamp.com>
2020-01-14 14:24:38 +0100

    Ues ES6 imports for Cesium

--- rollup.config.js
+++ rollup.config.js
@@ -4,49 +4,44 @@ import {terser} from "rollup-plugin-terser";
 import copy from 'rollup-plugin-copy'
 import babel from 'rollup-plugin-babel';
 import serve from 'rollup-plugin-serve'
+import alias from '@rollup/plugin-alias';
+// import rollupStripPragma from 'rollup-plugin-strip-pragma';
 
+const cesiumSource = 'node_modules/cesium/Source';
+const cesiumWorkers = '../Build/Cesium/Workers';
 
 const config = {
   input: 'src/index.js',
-  output: [
-  {
+  output: [{
     file: 'dist/bundle.debug.js',
     sourcemap: true,
     format: 'esm',
-  },
-  {
-    file: 'dist/bundle.min.js',
-    sourcemap: true,
-    format: 'esm',
-    plugins: [
-      terser(),
-    ]
   }],
   plugins: [
+    alias({
+      entries: [
+        {
+          find: 'cesium', replacement: cesiumSource,
+        }
+      ]
+    }),
+    // disabled since it breaks sourcemaps
+    // rollupStripPragma({
+    //   pragmas: ['debug']
+    // }),
     resolve(),
     commonjs(),
     copy({
       targets: [
         { src: 'index.html', dest: 'dist/' },
-        { src: 'src', dest: 'dist/' }
+        { src: 'src', dest: 'dist/' },
+        { src: cesiumSource + '/' + cesiumWorkers, dest: 'dist/' },
+        { src: cesiumSource + '/Assets', dest: 'dist/'},
+        { src: cesiumSource + '/Widgets', dest: 'dist/'},
+        { src: cesiumSource + '/ThirdParty/', dest: 'dist/'},
       ]
     }),
-    babel({
-      externalHelpers: false,
-      babelrc: false,
-      presets: [
-        [
-          "@babel/preset-env",
-          {
-            //debug: true, // disable to get debug information
-            modules: false,
-            useBuiltIns: 'usage', // does it make sense?
-            corejs:  { version: 2, proposals: false },
-          }
-        ]
-      ]
-    //  exclude: 'node_modules/**'
-    }),
+
   ],
 };
 
@@ -57,5 +52,34 @@ if (process.env.SERVE) {
   }));
 }
 
+if (process.env.mode === 'production') {
+  config.plugins.push(...[
+    babel({
+    externalHelpers: false,
+    babelrc: false,
+    presets: [
+      [
+        "@babel/preset-env",
+        {
+          //debug: true, // disable to get debug information
+          modules: false,
+          useBuiltIns: 'usage', // does it make sense?
+          corejs:  { version: 2, proposals: false },
+        }
+      ]
+    ],
+     // exclude: 'node_modules/**'
+   }), 
+  ]);
+
+  config.output.push({
+    file: 'dist/bundle.min.js',
+    sourcemap: true,
+    format: 'esm',
+    plugins: [
+      terser(),
+    ]
+  })
+}
 
 export default config;
--- src/NavigableVolumeLimiter.js
+++ src/NavigableVolumeLimiter.js
@@ -1,3 +1,5 @@
+import { BoundingSphere, Ellipsoid, Cartesian3, Cartographic } from "cesium/Cesium.js";
+
 /**
  * Constrain the camera so that it stays close to the bounding sphere of the map extent.
  * Near the ground the allowed distance is shorter.
@@ -5,7 +7,7 @@
 export default class NavigableVolumeLimiter {
   constructor(scene, rectangle, height, ratioFunction) {
     this.blockLimiter_ = false;
-    this.boundingSphere_ = Cesium.BoundingSphere.fromRectangle3D(rectangle, Cesium.Ellipsoid.WGS84, height);
+    this.boundingSphere_ = BoundingSphere.fromRectangle3D(rectangle, Ellipsoid.WGS84, height);
     this.ratioFunction_ = ratioFunction;
     scene.postRender.addEventListener(() => this.limit_(scene), scene);
   }
@@ -14,9 +16,9 @@ export default class NavigableVolumeLimiter {
     if (this.boundingSphere_ && !this.blockLimiter_) {
       const camera = scene.camera;
       const position = camera.position;
-      const carto = Cesium.Cartographic.fromCartesian(position);
+      const carto = Cartographic.fromCartesian(position);
       const ratio = this.ratioFunction_(carto.height);
-      if (Cesium.Cartesian3.distance(this.boundingSphere_.center, position) > this.boundingSphere_.radius * ratio) {
+      if (Cartesian3.distance(this.boundingSphere_.center, position) > this.boundingSphere_.radius * ratio) {
         const currentlyFlying = camera.flying;
         if (currentlyFlying === true) {
           // There is a flying property and its value is true
