0aa0b33842349d300d523040c0753e074698cf24
vladyslavtk <v.tkachenko@mobilunity.com>
2020-02-11 17:32:33 +0200

    click on area

--- src/AreaOfInterestDrawer.js
+++ src/AreaOfInterestDrawer.js
@@ -7,6 +7,8 @@ import Rectangle from 'cesium/Core/Rectangle.js';
 import Cartesian3 from 'cesium/Core/Cartesian3.js';
 import Ellipsoid from 'cesium/Core/Ellipsoid.js';
 import CallbackProperty from 'cesium/DataSources/CallbackProperty.js';
+import CustomDataSource from 'cesium/DataSources/CustomDataSource';
+import defined from 'cesium/Core/defined';
 
 export default class AreaOfInterestDrawer {
   constructor(viewer) {
@@ -19,8 +21,8 @@ export default class AreaOfInterestDrawer {
     // viewer.imageryLayers.get(0).brightness = 0.7;
 
     this.viewer_ = viewer;
-    this.selector_ = null;
-    this.rectangleSelector_ = new Rectangle();
+    this.area_ = null;
+    this.areaRectangle_ = new Rectangle();
     this.screenSpaceEventHandler_ = new ScreenSpaceEventHandler(this.viewer_.scene.canvas);
     this.cartesian_ = new Cartesian3();
     this.tempCartographic_ = new Cartographic();
@@ -29,39 +31,43 @@ export default class AreaOfInterestDrawer {
     this.mouseDown_ = false;
     this.camera_ = this.viewer_.camera;
 
-    //Draw the selector while the user drags the mouse while holding shift
-    this.screenSpaceEventHandler_.setInputAction(this.drawSelector.bind(this, ...arguments), ScreenSpaceEventType.MOUSE_MOVE, KeyboardEventModifier.SHIFT);
+    this.screenSpaceEventHandler_.setInputAction(this.drawArea.bind(this, ...arguments), ScreenSpaceEventType.MOUSE_MOVE, KeyboardEventModifier.SHIFT);
 
-    this.getSelectorLocation = new CallbackProperty(this.getSelectorLocationCallback.bind(this, ...arguments), false);
+    this.getAreaLocation = new CallbackProperty(this.getAreaLocationCallback.bind(this, ...arguments), false);
 
-    this.screenSpaceEventHandler_.setInputAction(this.startClickShift.bind(this), ScreenSpaceEventType.LEFT_DOWN, KeyboardEventModifier.SHIFT);
+    this.screenSpaceEventHandler_.setInputAction(this.startDrawing.bind(this), ScreenSpaceEventType.LEFT_DOWN, KeyboardEventModifier.SHIFT);
 
-    this.screenSpaceEventHandler_.setInputAction(this.endClickShift.bind(this), ScreenSpaceEventType.LEFT_UP, KeyboardEventModifier.SHIFT);
+    this.screenSpaceEventHandler_.setInputAction(this.endDrawing.bind(this), ScreenSpaceEventType.LEFT_UP, KeyboardEventModifier.SHIFT);
 
-    //Hide the selector by clicking anywhere
-    this.screenSpaceEventHandler_.setInputAction(this.hideSelector.bind(this), ScreenSpaceEventType.LEFT_CLICK);
+    this.screenSpaceEventHandler_.setInputAction(this.onClick.bind(this, ...arguments), ScreenSpaceEventType.LEFT_CLICK);
+
+    this.interestAreasDataSource = new CustomDataSource('interestAreas');
+    this.viewer_.dataSources.add(this.interestAreasDataSource);
+    this.interestAreasDataSource.entities.collectionChanged.addEventListener(() => {
+      this.viewer_.scene.requestRender();
+    });
   }
 
-  startClickShift() {
+  startDrawing() {
     this.viewer_.scene.screenSpaceCameraController.enableTranslate = false;
     this.viewer_.scene.screenSpaceCameraController.enableTilt = false;
     this.viewer_.scene.screenSpaceCameraController.enableLook = false;
     // this.viewer_.scene.screenSpaceCameraController.enableCollisionDetection = true;
 
-    this.selector_ = this.viewer_.entities.add({
+    this.area_ = this.interestAreasDataSource.entities.add({
       selectable: false,
       show: false,
       rectangle: {
-        coordinates: this.getSelectorLocation,
+        coordinates: this.getAreaLocation,
         material: Color.BLACK.withAlpha(0.5)
       }
     });
 
     this.mouseDown_ = true;
-    this.selector_.rectangle.coordinates = this.getSelectorLocation;
+    this.area_.rectangle.coordinates = this.getAreaLocation;
   }
 
-  endClickShift() {
+  endDrawing() {
     this.viewer_.scene.screenSpaceCameraController.enableTranslate = true;
     this.viewer_.scene.screenSpaceCameraController.enableTilt = true;
     this.viewer_.scene.screenSpaceCameraController.enableLook = true;
@@ -69,19 +75,19 @@ export default class AreaOfInterestDrawer {
 
     this.mouseDown_ = false;
     this.firstPointSet_ = false;
-    this.selector_.rectangle.coordinates = this.rectangleSelector_;
+    this.area_.rectangle.coordinates = this.areaRectangle_;
   }
 
 
-  hideSelector() {
-    // this.selector_.show = false;
+  removeAreas() {
+    // this.area_.show = false;
   }
 
-  getSelectorLocationCallback(time, result) {
-    return Rectangle.clone(this.rectangleSelector_, result);
+  getAreaLocationCallback(time, result) {
+    return Rectangle.clone(this.areaRectangle_, result);
   }
 
-  drawSelector(viewer, movement) {
+  drawArea(viewer, movement) {
     if (!this.mouseDown_) {
       return;
     }
@@ -89,19 +95,27 @@ export default class AreaOfInterestDrawer {
     this.cartesian_ = this.camera_.pickEllipsoid(movement.endPosition, this.viewer_.scene.globe.ellipsoid, this.cartesian_);
 
     if (this.cartesian_) {
-      //mouse cartographic
       this.tempCartographic_ = Cartographic.fromCartesian(this.cartesian_, Ellipsoid.WGS84, this.tempCartographic_);
 
       if (!this.firstPointSet_) {
         Cartographic.clone(this.tempCartographic_, this.firstPoint_);
         this.firstPointSet_ = true;
       } else {
-        this.rectangleSelector_.east = Math.max(this.tempCartographic_.longitude, this.firstPoint_.longitude);
-        this.rectangleSelector_.west = Math.min(this.tempCartographic_.longitude, this.firstPoint_.longitude);
-        this.rectangleSelector_.north = Math.max(this.tempCartographic_.latitude, this.firstPoint_.latitude);
-        this.rectangleSelector_.south = Math.min(this.tempCartographic_.latitude, this.firstPoint_.latitude);
-        this.selector_.show = true;
+        this.areaRectangle_.east = Math.max(this.tempCartographic_.longitude, this.firstPoint_.longitude);
+        this.areaRectangle_.west = Math.min(this.tempCartographic_.longitude, this.firstPoint_.longitude);
+        this.areaRectangle_.north = Math.max(this.tempCartographic_.latitude, this.firstPoint_.latitude);
+        this.areaRectangle_.south = Math.min(this.tempCartographic_.latitude, this.firstPoint_.latitude);
+        this.area_.show = true;
+        this.area_.selectable = true;
       }
     }
+  }
+
+  onClick(viewer, movement) {
+    const pickedObject = this.viewer_.scene.pick(movement.position);
+    if (defined(pickedObject) && this.interestAreasDataSource.entities.contains(pickedObject.id)) {
+      this.interestAreasDataSource.entities.getById(pickedObject.id.id).rectangle.material = Color.YELLOW.withAlpha(0.5);
+    }
+
   }
 }
