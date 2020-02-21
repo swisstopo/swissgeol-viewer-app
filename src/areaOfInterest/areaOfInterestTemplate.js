73ffdd5797dc3480a8392820f96faea530895042
vladyslavtk <v.tkachenko@mobilunity.com>
2020-02-21 16:42:51 +0200

    translation fix

--- src/areaOfInterest/areaOfInterestTemplate.js
+++ src/areaOfInterest/areaOfInterestTemplate.js
@@ -4,22 +4,22 @@ import i18next from 'i18next';
 import {onAccordionTitleClick} from './helpers.js';
 import {clickOnElement} from '../utils.js';
 
-const tooltipTranslationLabel = 'area_of_interest_add_hint';
-const hintTranslationLabel = 'area_of_interest_empty_hint';
-const btnTranslation = {
-  cancel: 'cancel_area_btn_label',
-  removeAll: 'remove_all_area_btn_label',
-  add: 'add_area_btn_label',
-  hide: 'hide_btn_tooltip',
-  remove: 'remove_btn_tooltip',
-  flyTo: 'fly_to_btn_tooltip',
-  unhide: 'unhide_btn_tooltip',
-  upload: 'upload_btn_label'
-};
-
 const areaUploadInputId = 'areaUpload';
 
 export default function getTemplate() {
+  const tooltipTranslationLabel = i18next.t('area_of_interest_add_hint');
+  const hintTranslationLabel = i18next.t('area_of_interest_empty_hint');
+  const btnTranslation = {
+    cancel: i18next.t('cancel_area_btn_label'),
+    removeAll: i18next.t('remove_all_area_btn_label'),
+    add: i18next.t('add_area_btn_label'),
+    hide: i18next.t('hide_btn_tooltip'),
+    remove: i18next.t('remove_btn_tooltip'),
+    flyTo: i18next.t('fly_to_btn_tooltip'),
+    unhide: i18next.t('unhide_btn_tooltip'),
+    upload: i18next.t('upload_btn_label')
+  };
+
   return html`
   <div class="title" @click=${onAccordionTitleClick}>
     <i class="dropdown icon"></i>
@@ -27,25 +27,24 @@ export default function getTemplate() {
   </div>
   <div class="content">
     <div class="ui tiny fluid buttons ngm-new-aoi-container" ?hidden=${this.drawMode_}>
-        <button class="ui blue button" @click=${this.onAddAreaClick_.bind(this)} data-i18n>
-            <i class="plus icon"></i>${i18next.t(btnTranslation.add)}
+        <button class="ui blue button" @click=${this.onAddAreaClick_.bind(this)}>
+            <i class="plus icon"></i>${btnTranslation.add}
         </button>
-        <button class="ui blue button" @click=${clickOnElement.bind(null, areaUploadInputId)} data-i18n>
-            <i class="file upload icon"></i>${i18next.t(btnTranslation.upload)}
+        <button class="ui blue button" @click=${clickOnElement.bind(null, areaUploadInputId)}>
+            <i class="file upload icon"></i>${btnTranslation.upload}
         </button>
     </div>
     <button class="ui tiny red fluid button"
             @click=${this.onRemoveEntityClick_.bind(this, null)}
-            data-i18n
+
             ?hidden=${this.drawMode_}>
-            <i class="trash alternate outline icon"></i>${i18next.t(btnTranslation.removeAll)}
+            <i class="trash alternate outline icon"></i>${btnTranslation.removeAll}
     </button>
     <input id="${areaUploadInputId}" type='file' accept=".kml,.KML" hidden @change=${this.uploadArea_.bind(this)} />
     <div class="ui tiny basic fluid buttons ngm-aoi-tooltip-container" ?hidden=${!this.drawMode_}>
-        <button class="ui button" @click=${this.cancelDraw_.bind(this)} data-i18n>${i18next.t(btnTranslation.cancel)}</button>
+        <button class="ui button" @click=${this.cancelDraw_.bind(this)}>${btnTranslation.cancel}</button>
         <button class="ui button ngm-help-btn"
-                data-i18n
-                data-tooltip=${i18next.t(tooltipTranslationLabel)}
+                data-tooltip=${tooltipTranslationLabel}
                 data-variation="tiny"
                 data-position="top right">
             <i class="question circle outline icon"></i>
@@ -60,24 +59,21 @@ export default function getTemplate() {
             <button
             class="ui button"
             @click=${this.flyToArea_.bind(this, i.id)}
-            data-i18n
-            data-tooltip=${i18next.t(btnTranslation.flyTo)}
+            data-tooltip=${btnTranslation.flyTo}
             data-position="top center"
             data-variation="tiny"
             ><i class="map marked alternate icon"></i></button>
             <button
             class="ui button"
             @click=${this.onShowHideEntityClick_.bind(this, i.id)}
-            data-i18n
-            data-tooltip=${i18next.t(i.show ? btnTranslation.hide : btnTranslation.unhide)}
+            data-tooltip=${i.show ? btnTranslation.hide : btnTranslation.unhide}
             data-position="top center"
             data-variation="tiny"
             ><i class="${i.show ? 'eye outline' : 'eye slash outline'} icon"></i></button>
             <button
             class="ui button"
             @click=${this.onRemoveEntityClick_.bind(this, i.id)}
-            data-i18n
-            data-tooltip=${i18next.t(btnTranslation.remove)}
+            data-tooltip=${btnTranslation.remove}
             data-position="top center"
             data-variation="tiny"
             ><i class="trash alternate outline icon"></i></button>
@@ -85,7 +81,7 @@ export default function getTemplate() {
       </div>`)}
     </div>
     <div ?hidden=${this.entitiesList_ && this.entitiesList_.length} class="ui tertiary center aligned segment">
-        <span data-i18n>${i18next.t(hintTranslationLabel)}</span>
+        <span>${hintTranslationLabel}</span>
     </div>
   </div>
   `;
