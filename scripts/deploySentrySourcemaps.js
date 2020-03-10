7107113bed5a5138429276dcd47304c0baec00d9
vladyslavtk <v.tkachenko@mobilunity.com>
2020-03-10 17:02:53 +0200

    move sentry deploy to separate script

---
+++ scripts/deploySentrySourcemaps.js
@@ -0,0 +1,21 @@
+const util = require('util');
+const environment = require('./environment.json');
+
+const execAsync = util.promisify(require('child_process').exec);
+
+(async () => {
+  try {
+    if (environment && environment.branch) {
+      console.log(`Uploading sourcemaps for ${environment.branch}...`);
+      await execAsync(`RELEASE=${environment.branch} npm run sentry-sourcemaps-upload`);
+      console.log('Sourcemaps uploaded.');
+      process.exit(0);
+    } else {
+      console.error('Branch name missing.');
+      process.exit(1);
+    }
+  } catch (e) {
+    console.error(`Failed with error: "${e.message}".`);
+    process.exit(1);
+  }
+})();
---
+++ scripts/environment.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const util = require('util');
+const path = require('path');
+
+const exec = util.promisify(require('child_process').exec);
+const writeFileAsync = util.promisify(fs.writeFile);
+const mkdirp = require('mkdirp');
+const mkdirpAsync = util.promisify(mkdirp);
+
+(async (branch) => {
+  try {
+    const dir = '../src/environments';
+    const dirNode = './environments';
+    const filename = 'environment';
+
+    if (!branch) {
+      branch = (await exec('git rev-parse --abbrev-ref HEAD')).stdout.toString().trim();
+    }
+
+    const jsonContent = `{"branch": "${branch}"}`;
+    const content = `export const environment = ${jsonContent};`;
+
+    await mkdirpAsync(path.resolve(__dirname, dir));
+    await mkdirpAsync(path.resolve(__dirname, dirNode));
+
+    await writeFileAsync(path.resolve(__dirname, `${dir}/${filename}.js`), content, {encoding: 'utf8'}); // for frontend
+    await writeFileAsync(path.resolve(__dirname, `${dirNode}/${filename}.json`), jsonContent, {encoding: 'utf8'}); // for node
+    process.exit(0);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+})(process.argv.slice(2)[0]);
