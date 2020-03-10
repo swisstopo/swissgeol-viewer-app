e2d9be0d78e912b953ec411488100761917b0f81
vladyslavtk <v.tkachenko@mobilunity.com>
2020-03-10 19:20:10 +0200

    remove user info from sentry

--- src/sentry.js
+++ src/sentry.js
@@ -2,5 +2,15 @@ import * as Sentry from '@sentry/browser';
 import {environment} from './environments/environment.js';
 
 export function initSentry() {
-  Sentry.init({dsn: 'https://dc2f101d00734a4b95a459f46f17cbb0@sentry.io/3820326', release: environment.branch}); // TODO replace test dsn
+  Sentry.init({
+    dsn: 'https://dc2f101d00734a4b95a459f46f17cbb0@sentry.io/3820326',
+    release: environment.branch,
+    beforeSend: function (event) {
+      if (event.user) {
+        // Remove user info
+        delete event.user;
+      }
+      return event;
+    }
+  });
 }
