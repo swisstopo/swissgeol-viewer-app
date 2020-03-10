ecf303272865bb259848a8d58e37499243a4a8a7
vladyslavtk <v.tkachenko@mobilunity.com>
2020-03-10 12:59:49 +0200

    sentry sourcemaps

--- src/sentry.js
+++ src/sentry.js
@@ -1,5 +1,7 @@
 import * as Sentry from '@sentry/browser';
 
 export function initSentry() {
-  Sentry.init({dsn: 'https://273d49ea5057418c89b8160c68e9058d@sentry.io/3818111', release: 'test-app'}); // TODO replace test dsn
+  let release = location.host;
+  release = release.include('review') ? `${release}/${location.pathname.split('/')[1]}` : release; // TODO improve
+  Sentry.init({dsn: 'https://273d49ea5057418c89b8160c68e9058d@sentry.io/3818111', release: release}); // TODO replace test dsn
 }
