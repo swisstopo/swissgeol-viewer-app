89f390d2d5a84c5fce7956b53840128ef934cc4d
vladyslavtk <v.tkachenko@mobilunity.com>
2020-03-10 19:06:16 +0200

    use ngm creds

--- src/sentry.js
+++ src/sentry.js
@@ -2,5 +2,5 @@ import * as Sentry from '@sentry/browser';
 import {environment} from './environments/environment.js';
 
 export function initSentry() {
-  Sentry.init({dsn: 'https://273d49ea5057418c89b8160c68e9058d@sentry.io/3818111', release: environment.branch}); // TODO replace test dsn
+  Sentry.init({dsn: 'https://dc2f101d00734a4b95a459f46f17cbb0@sentry.io/3820326', release: environment.branch}); // TODO replace test dsn
 }
