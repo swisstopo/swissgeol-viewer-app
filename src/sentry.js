16492b9a10c0e6bc040cf66c97d056ee08e84bc0
vladyslavtk <v.tkachenko@mobilunity.com>
2020-03-11 10:51:31 +0200

    disable sentry for localhost

--- src/sentry.js
+++ src/sentry.js
@@ -5,6 +5,7 @@ export function initSentry() {
   Sentry.init({
     dsn: 'https://dc2f101d00734a4b95a459f46f17cbb0@sentry.io/3820326',
     release: environment.branch,
+    enabled: !location.host.includes('localhost'),
     beforeSend: function (event) {
       if (event.user) {
         // Remove user info
