7618e5b865d0fde10d522c07cf08ac1bd2d0cbe9
Frederic Junod <frederic.junod@camptocamp.com>
2020-03-21 08:00:18 +0100

    Always create the versions.json file

--- scripts/deploy_to_prod.sh
+++ scripts/deploy_to_prod.sh
@@ -12,13 +12,6 @@ git checkout prod
 git reset --hard origin/prod
 git pull origin $VERSION
 make dist
-cat > dist/versions.json <<EOF
-{
-  'tag': '$PROD_TAG',
-  'commit_hash': '`git rev-list HEAD -1`',
-  'cesium': '`grep '"version"' node_modules/cesium/package.json| cut -f4 -d\"`'
-}
-EOF
 scripts/deploy_to_s3.sh prod
 git tag $PROD_TAG -m $PROD_TAG
 git push origin $PROD_TAG
