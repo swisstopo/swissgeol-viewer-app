e9713ea9b06ac1ecf54c185e79320775fb78a23c
Frederic Junod <frederic.junod@camptocamp.com>
2020-04-15 09:02:22 +0200

    map-choose style simplification and fine tuning

--- src/elements/map-chooser/ngm-map-chooser-style.js
+++ src/elements/map-chooser/ngm-map-chooser-style.js
@@ -6,7 +6,6 @@ const style = css`
 }
 
 img {
-  padding: 0 5px;
   width: 64px;
   height: 64px;
 }
@@ -15,19 +14,25 @@ label {
   font-size: 11px;
   position: absolute;
   background-color: white;
-  padding: 0 5px;
   width: 64px;
 }
 
 .ngm-map-preview {
+  cursor: pointer;
+  padding: 0 5px;
   background-color: white;
   text-align: center;
   margin-right: 4px;
   border: 1px solid lightgrey;
 }
 
+.ngm-map-preview:last-child {
+  margin-right: 0;
+}
+
 .ngm-map-preview.active {
-  border: 2px solid red;
+  border: 1px solid red;
+  outline: 1px solid red;
 }
 `;
 
