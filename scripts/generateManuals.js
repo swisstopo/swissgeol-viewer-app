a73e7032f4e755b9b0ee6b49f4e97f15f105bf14
vladyslavtk <v.tkachenko@mobilunity.com>
2020-05-08 17:08:40 +0300

    checks for missing / extra tags

--- scripts/generateManuals.js
+++ scripts/generateManuals.js
@@ -1,30 +1,44 @@
 const fs = require('fs');
-const util = require('util');
 const path = require('path');
 
-const readFileAsync = util.promisify(fs.readFile);
-const writeFileAsync = util.promisify(fs.writeFile);
-
 const languages = ['en', 'de'];
+const tagPrefix = 'localetag_'; // update html if changed
 
 (async () => {
   try {
-    const buffer = await readFileAsync(path.resolve(__dirname, '../manuals/manual_base.html'));
+    const buffer = await fs.readFileSync(path.resolve(__dirname, '../manuals/manual_base.html'));
     await Promise.all(languages.map(async (lang) => {
       console.log(`Generating manual html for ${lang}`);
 
       let html = buffer.toString('utf8');
       const manualLocale = require(path.resolve(__dirname, `../manuals/locales/${lang}.json`));
       manualLocale.forEach(t => {
-        html = html.replace(new RegExp(`${t.tag}(?!(_|[a-zA-Z]))`, 'gi'), t.text);
+        if (html.includes(`${tagPrefix}${t.tag}`)) {
+          html = html.replace(new RegExp(`${tagPrefix}${t.tag}(?!(_|[a-zA-Z]))`, 'gi'), t.text);
+          t.used = true;
+        }
       });
+
+      const notUsedLocales = manualLocale.filter(locale => !locale.used);
+      if (notUsedLocales.length) {
+        const notUsedTags = notUsedLocales.map(l => l.tag);
+        console.error('HTML not generated. Reason:');
+        console.error(`Not used tags: ${notUsedTags.join(', ')}. Language: ${lang}`);
+        process.exit(1);
+      }
+
+      if (html.includes(tagPrefix)) {
+        console.error('HTML not generated. Reason: ');
+        console.error(`Not all tags replaced in HTML. Language: ${lang}`);
+        process.exit(1);
+      }
 
       const dir = path.resolve(__dirname, '../manuals/dist');
       if (!fs.existsSync(dir)) {
         fs.mkdirSync(dir);
       }
 
-      return await writeFileAsync(path.resolve(__dirname, `${dir}/manual_${lang}.html`), html);
+      return await fs.writeFileSync(path.resolve(__dirname, `${dir}/manual_${lang}.html`), html);
     }));
     process.exit(0);
   } catch (e) {
