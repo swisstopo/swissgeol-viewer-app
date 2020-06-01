0e749266cb4e64c49be74e607af621e34fe9faf4
vladyslavtk <v.tkachenko@mobilunity.com>
2020-06-01 16:32:47 +0300

    minor update

--- src/viewer.js
+++ src/viewer.js
@@ -37,6 +37,43 @@ Object.assign(RequestScheduler.requestsByServer, {
 
 let noLimit;
 
+const FOG_FRAGMENT_SHADER_SOURCE =
+  'float getDistance(sampler2D depthTexture, vec2 texCoords) \n' +
+  '{ \n' +
+  '    float depth = czm_unpackDepth(texture2D(depthTexture, texCoords)); \n' +
+  '    if (depth == 0.0) { \n' +
+  '        return czm_infinity; \n' +
+  '    } \n' +
+  '    vec4 eyeCoordinate = czm_windowToEyeCoordinates(gl_FragCoord.xy, depth); \n' +
+  '    return -eyeCoordinate.z / eyeCoordinate.w; \n' +
+  '} \n' +
+  'float interpolateByDistance(vec4 nearFarScalar, float distance) \n' +
+  '{ \n' +
+  '    float startDistance = nearFarScalar.x; \n' +
+  '    float startValue = nearFarScalar.y; \n' +
+  '    float endDistance = nearFarScalar.z; \n' +
+  '    float endValue = nearFarScalar.w; \n' +
+  '    float t = clamp((distance - startDistance) / (endDistance - startDistance), 0.0, 1.0); \n' +
+  '    return mix(startValue, endValue, t); \n' +
+  '} \n' +
+  'vec4 alphaBlend(vec4 sourceColor, vec4 destinationColor) \n' +
+  '{ \n' +
+  '    return sourceColor * vec4(sourceColor.aaa, 1.0) + destinationColor * (1.0 - sourceColor.a); \n' +
+  '} \n' +
+  'uniform sampler2D colorTexture; \n' +
+  'uniform sampler2D depthTexture; \n' +
+  'uniform vec4 fogByDistance; \n' +
+  'uniform vec4 fogColor; \n' +
+  'varying vec2 v_textureCoordinates; \n' +
+  'void main(void) \n' +
+  '{ \n' +
+  '    float distance = getDistance(depthTexture, v_textureCoordinates); \n' +
+  '    vec4 sceneColor = texture2D(colorTexture, v_textureCoordinates); \n' +
+  '    float blendAmount = interpolateByDistance(fogByDistance, distance); \n' +
+  '    vec4 undergroundColor = vec4(fogColor.rgb, fogColor.a * blendAmount); \n' +
+  '    gl_FragColor = alphaBlend(undergroundColor, sceneColor); \n' +
+  '} \n';
+
 /**
  * @param {HTMLElement} container
  */
@@ -140,59 +177,22 @@ export function setupViewer(container) {
   const transparency = transparencyParam ? 1 - transparencyParam : 0.6;
   globe.translucencyEnabled = transparency !== 1;
   globe.frontFaceAlphaByDistance = new NearFarScalar(10000, transparency, 50000, 1.0);
-  globe.undergroundColorByDistance = new NearFarScalar(6000, 0.1, 500000, 1.0);
+  // globe.undergroundColorByDistance = new NearFarScalar(6000, 0.1, 500000, 1.0);
   globe.backFaceAlpha = transparency === 1 ? 1 : 0;
 
-  // scene.postRender.addEventListener((scene) => {
-  //   scene.skyAtmosphere.show = !scene.cameraUnderground;
-  // });
+  const fog = new PostProcessStage({
+    fragmentShader: FOG_FRAGMENT_SHADER_SOURCE,
+    uniforms: {
+      fogByDistance: new Cartesian4(10, 0.0, 250000, 1.0),
+      fogColor: Color.BLACK,
+    },
+  });
 
-  const fragmentShaderSource =
-    'float getDistance(sampler2D depthTexture, vec2 texCoords) \n' +
-    '{ \n' +
-    '    float depth = czm_unpackDepth(texture2D(depthTexture, texCoords)); \n' +
-    '    if (depth == 0.0) { \n' +
-    '        return czm_infinity; \n' +
-    '    } \n' +
-    '    vec4 eyeCoordinate = czm_windowToEyeCoordinates(gl_FragCoord.xy, depth); \n' +
-    '    return -eyeCoordinate.z / eyeCoordinate.w; \n' +
-    '} \n' +
-    'float interpolateByDistance(vec4 nearFarScalar, float distance) \n' +
-    '{ \n' +
-    '    float startDistance = nearFarScalar.x; \n' +
-    '    float startValue = nearFarScalar.y; \n' +
-    '    float endDistance = nearFarScalar.z; \n' +
-    '    float endValue = nearFarScalar.w; \n' +
-    '    float t = clamp((distance - startDistance) / (endDistance - startDistance), 0.0, 1.0); \n' +
-    '    return mix(startValue, endValue, t); \n' +
-    '} \n' +
-    'vec4 alphaBlend(vec4 sourceColor, vec4 destinationColor) \n' +
-    '{ \n' +
-    '    return sourceColor * vec4(sourceColor.aaa, 1.0) + destinationColor * (1.0 - sourceColor.a); \n' +
-    '} \n' +
-    'uniform sampler2D colorTexture; \n' +
-    'uniform sampler2D depthTexture; \n' +
-    'uniform vec4 fogByDistance; \n' +
-    'uniform vec4 fogColor; \n' +
-    'varying vec2 v_textureCoordinates; \n' +
-    'void main(void) \n' +
-    '{ \n' +
-    '    float distance = getDistance(depthTexture, v_textureCoordinates); \n' +
-    '    vec4 sceneColor = texture2D(colorTexture, v_textureCoordinates); \n' +
-    '    float blendAmount = interpolateByDistance(fogByDistance, distance); \n' +
-    '    vec4 undergroundColor = vec4(fogColor.rgb, fogColor.a * blendAmount); \n' +
-    '    gl_FragColor = alphaBlend(undergroundColor, sceneColor); \n' +
-    '} \n';
+  viewer.scene.postProcessStages.add(fog);
 
-  viewer.scene.postProcessStages.add(
-    new PostProcessStage({
-      fragmentShader: fragmentShaderSource,
-      uniforms: {
-        fogByDistance: new Cartesian4(10, 0.0, 200000, 1.0),
-        fogColor: Color.BLACK,
-      },
-    })
-  );
+  scene.postRender.addEventListener((scene) => {
+    fog.enabled = scene.cameraUnderground;
+  });
 
   setupBaseLayers(viewer);
 
