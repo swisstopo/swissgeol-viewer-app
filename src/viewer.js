c3785097fd308fe0ccb8b1b35f41626689a01f67
vladyslavtk <v.tkachenko@mobilunity.com>
2020-06-02 21:30:37 +0300

    fix permalink

--- src/viewer.js
+++ src/viewer.js
@@ -174,7 +174,7 @@ export function setupViewer(container) {
   // camera is 10000 meters from the surface and 1.0
   // as the camera distance approaches 50000 meters.
   const transparencyParam = getMapTransparencyParam();
-  const transparency = transparencyParam ? 1 - transparencyParam : 0.6;
+  const transparency = !isNaN(transparencyParam) ? 1 - transparencyParam : 0.6;
   globe.translucencyEnabled = transparency !== 1;
   globe.frontFaceAlphaByDistance = new NearFarScalar(10000, transparency, 50000, 1.0);
   // globe.undergroundColorByDistance = new NearFarScalar(6000, 0.1, 500000, 1.0);
