8a17d2ed050639da47ef4b9b7a9d446c4c54c13e
Frederic Junod <frederic.junod@camptocamp.com>
2020-06-18 14:43:39 +0200

    Better code indentation

--- src/elements/ngm-slow-loading.js
+++ src/elements/ngm-slow-loading.js
@@ -14,24 +14,24 @@ class NgmSlowLoading extends I18nMixin(LitElement) {
 
   render() {
     return html`
-    <div class="ui inverted segment">
+      <div class="ui inverted segment">
         <div class="ui inverted accordion">
           <div class="title">
             <i class="dropdown icon"></i>
             ${i18next.t('slow_connection_label')}
           </div>
-          <div class="content ${!this.hideReviewWindow ? 'active' : ''}">
-          <p>${i18next.t('slow_connection_text')}</p>
-          <p>${i18next.t('slow_connection_text_2')}</p>
-          <ul>
-            <li>Edge (${i18next.t('latest_stable_version')})</li>
-            <li>Chrome (${i18next.t('latest_stable_version')})</li>
-            <li>Safari (${i18next.t('latest_stable_version')})</li>
-            <li>Firefox (${i18next.t('latest_stable_version')})</li>
-          </ul>
+          <div class="content active}">
+            <p>${i18next.t('slow_connection_text')}</p>
+            <p>${i18next.t('slow_connection_text_2')}</p>
+            <ul>
+              <li>Edge (${i18next.t('latest_stable_version')})</li>
+              <li>Chrome (${i18next.t('latest_stable_version')})</li>
+              <li>Safari (${i18next.t('latest_stable_version')})</li>
+              <li>Firefox (${i18next.t('latest_stable_version')})</li>
+            </ul>
           </div>
         </div>
-     </div>
+      </div>
     `;
   }
 
