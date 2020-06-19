c483a3be3396ca2ab30d7cc053097837fd5fa1dd
Frederic Junod <frederic.junod@camptocamp.com>
2020-06-19 17:02:19 +0200

    Adapt CopyPlugin call to the new API

--- webpack.config.js
+++ webpack.config.js
@@ -49,23 +49,25 @@ module.exports = smp.wrap({
     port: 8000
   },
     plugins: [
-      new CopyPlugin([
-        // FIXME: is there a less ugly way to write these rules?
-        { from: 'index.html', to: './' },
-        { from: 'src/', to: 'src/' },
-        { from: 'locales/', to: './locales/' },
-        { from: cesiumSource + '/' + cesiumWorkers, to: 'Workers/' },
-        { from: cesiumSource + '/Assets/', to: 'Assets/' },
-        { from: cesiumSource + '/Widgets/', to: 'Widgets/' },
-        { from: cesiumSource + '/ThirdParty/', to: 'ThirdParty/' },
-        { from: 'src/images/', to: 'images/' },
-        { from: 'node_modules/typeface-source-sans-pro/files/*', flatten: true, to: 'fonts/' },
-        { from: 'node_modules/fomantic-ui-css/themes/default/assets/fonts/*', flatten: true, to: 'fonts/' },
-        { from: 'node_modules/@webcomponents/webcomponentsjs/*', flatten: true, to: 'webcomponentsjs/' },
-        { from: 'manuals/dist/', to: './manuals/' },
-        { from: 'manuals/style.css', to: './manuals/' },
-        { from: 'manuals/images', to: './manuals/images/' },
-      ]),
+      new CopyPlugin({
+        patterns: [
+          // FIXME: is there a less ugly way to write these rules?
+          { from: 'index.html', to: './' },
+          { from: 'src/', to: 'src/' },
+          { from: 'locales/', to: './locales/' },
+          { from: cesiumSource + '/' + cesiumWorkers, to: 'Workers/' },
+          { from: cesiumSource + '/Assets/', to: 'Assets/' },
+          { from: cesiumSource + '/Widgets/', to: 'Widgets/' },
+          { from: cesiumSource + '/ThirdParty/', to: 'ThirdParty/' },
+          { from: 'src/images/', to: 'images/' },
+          { from: 'node_modules/typeface-source-sans-pro/files/*', flatten: true, to: 'fonts/' },
+          { from: 'node_modules/fomantic-ui-css/themes/default/assets/fonts/*', flatten: true, to: 'fonts/' },
+          { from: 'node_modules/@webcomponents/webcomponentsjs/*', flatten: true, to: 'webcomponentsjs/' },
+          { from: 'manuals/dist/', to: './manuals/' },
+          { from: 'manuals/style.css', to: './manuals/' },
+          { from: 'manuals/images', to: './manuals/images/' },
+        ]
+      }),
       new MiniCssExtractPlugin({
         filename: 'bundle.css'
       }),
