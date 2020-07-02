cc4f6ce028c231aac9417049c84291ba0f6f47b2
vladyslavtk <v.tkachenko@mobilunity.com>
2020-07-02 11:44:43 +0300

    pin object info header

--- src/elements/ngm-object-information.js
+++ src/elements/ngm-object-information.js
@@ -45,30 +45,28 @@ class NgmObjectInformation extends I18nMixin(LitElement) {
       return html`
         <div class="ui segment" ?hidden="${!this.opened}">
           <div class="header">
-            <div style="flex: auto;"></div>
             <div class="ui horizontal link list">
               <a class="item" href="#"><i @click="${() => this.opened = false}" class="times icon"></i></a>
             </div>
-          </div>
-          <table class="ui compact small very basic table">
-            <thead ?hidden="${!this.info.zoom}">
-              <tr>
-                <th colspan="2">
-                  <button @click="${this.info.zoom}" class="ui right floated mini basic labeled icon button">
+            <div ?hidden="${!this.info.zoom}">
+              <button @click="${this.info.zoom}" class="ui right floated mini basic labeled icon button">
                     <i class="search plus icon"></i>${i18next.t('zoom_to_object')}
-                  </button>
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              ${Object.entries(this.info).filter(this.filterInfo).map(([key, value]) => html`
-                <tr class="top aligned">
-                  <td class="key">${key}</td>
-                  <td class="val">${value}</td>
-                </tr>
-              `)}
-            </tbody>
-          </table>
+              </button>
+            </div>
+          </div>
+          <div class="ui divider"></div>
+          <div class="content-container">
+            <table class="ui compact small very basic table">
+              <tbody>
+                ${Object.entries(this.info).filter(this.filterInfo).map(([key, value]) => html`
+                  <tr class="top aligned">
+                    <td class="key">${key}</td>
+                    <td class="val">${value}</td>
+                  </tr>
+                `)}
+              </tbody>
+            </table>
+          </div>
         </div>
       `;
     } else {
