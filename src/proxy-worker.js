30726f8e4a39914c191b2c9240dc4c47928a0504
Andrea Borghi <andrea.borghi@camptocamp.com>
2020-08-06 17:47:50 +0200

    wip

--- src/proxy-worker.js
+++ src/proxy-worker.js
@@ -1,8 +1,20 @@
 
-importScripts('/src/aws-sdk-2.716.0.min.js');
-importScripts('/src/amazon-cognito-identity.min.js');
 
-const VERSION = 'v37';
+// need to use another xml parser because domparser is not available in SW
+// see https://github.com/TobiasNickel/tXml
+importScripts('/src/tXml.min.js');
+
+// modified version to use fetch API instead of XmlHttpRequest
+// see: https://github.com/aws/aws-sdk-js/issues/1902
+importScripts('/src/aws-sdk-2.727.1.js');
+
+
+//importScripts('/src/aws-sdk-2.716.0.min.js');
+//importScripts('/src/amazon-cognito-identity.min.js');
+
+
+
+const VERSION = 'v54';
 
 // for debug use aws builtin logger
 AWS.config.logger = console;
@@ -32,55 +44,32 @@ function normalizeUrl(url) {
   return url;
 }
 
-/**
- * Get an object from a s3 bucket
- *
- * @param  {string} key - Object location in the bucket
- * @return {object}     - A promise containing the response
- */
-const getObject = key => {
-  return new Promise((resolve, reject) => {
-      AWS.S3.getObject({
-          Bucket: process.env.BUCKET_NAME, // Assuming this is an environment variable...
-          Key: key
-      }, (err, data) => {
-          if ( err ) reject(err)
-          else resolve(data)
-      })
-  })
-}
+
 
 if (typeof self === 'object') {
   log('In the sw');
-
-  function list(idToken) {
-
-    AWS.config.region = 'eu-central-1';
-
-    AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-        IdentityPoolId: 'eu-central-1:21355ebf-703b-44dd-8900-f8bc391b4bde',
-        Logins: {
-            'cognito-idp.eu-central-1.amazonaws.com/eu-central-1_5wXXpcDt8': idToken
-        }
-    });
-
-    AWS.config.credentials.get(function(err) {
-        if (err) return console.error(err);
-        else console.log(AWS.config.credentials);
-
-        var s3 = new AWS.S3({
-            apiVersion: '2006-03-01',
-            params: {Bucket: 'ngm-dev-authenticated-resources'}
-        });
-
-        s3.listObjects({Delimiter: '/'}, function(err, data) {
-            console.log(err, data)
-        });
-    });
+  /**
+   * Get an object from a s3 bucket
+   *
+   * @param  {string} key - Object location in the bucket
+   * @return {object}     - A promise containing the response
+   */
+  function getObject( key ) {
+    return new Promise((resolve, reject) => {
+        self.s3.getObject({
+            Key: key
+        }, (err, data) => {
+            if ( err ) reject(err)
+            else resolve(data)
+        })
+    })
   }
-
   function updateAwsCredentialsWithToken(idToken){
     AWS.config.region = 'eu-central-1'; // Region
+    self.s3 = new AWS.S3({
+      apiVersion: '2006-03-01',
+      params: {Bucket: 'ngm-dev-authenticated-resources'}
+    });
 
     AWS.config.credentials = new AWS.CognitoIdentityCredentials({
       IdentityPoolId: 'eu-central-1:21355ebf-703b-44dd-8900-f8bc391b4bde',
@@ -88,41 +77,21 @@ if (typeof self === 'object') {
           'cognito-idp.eu-central-1.amazonaws.com/eu-central-1_5wXXpcDt8': idToken
       }
     });
-
+    AWS.config.credentials.get();
     log(`idToken = ${idToken}`);
     log(`AWS.config.credentials = ${AWS.config.credentials}`);
     log(`AWS.config.region = ${AWS.config.region}`);
     log(`AWS.config.credentials.accessKeyId = ${AWS.config.credentials.accessKeyId}`);
     log(`AWS.config.credentials.identityId = ${AWS.config.credentials.identityId}`);
-    self.s3 = new AWS.S3({
-      apiVersion: '2006-03-01',
-      params: {Bucket: 'ngm-dev-authenticated-resources'}
-    });
-    self.s3.listObjects({Delimiter: '/'}, function(err, data) {
-      console.log(err, data)
-    });
   }
 
   self.addEventListener('install', event => {
       log("INSTALLING ");
       const installCompleted = Promise.resolve()
                           .then(() => log("INSTALLED"));
-
+      //self.skipWaiting();
       event.waitUntil(installCompleted);
   });
-
-  // self.addEventListener('activate', event => {
-  //     log("ACTIVATING");
-  //     const activationCompleted = Promise.resolve()
-  //         .then((activationCompleted) => log("ACTIVATED"));
-  //     event.waitUntil(activationCompleted);
-  // });
-
-  // // Install the service worker (download website files)
-  // self.addEventListener('install', e => {
-  //   console.log('SW installation');
-  //   //e.waitUntil(downloadAndCacheWebsiteFiles());
-  // });
 
   // Immediately claim the clients so that they can go offline without reloading the page.
   self.addEventListener('activate', event => {
@@ -138,8 +107,10 @@ if (typeof self === 'object') {
   self.addEventListener('message', event => {
     if  (event.data.idToken) {
       log(`token received : ${event.data.idToken}`);
-      list(event.data.idToken);
-      //updateAwsCredentialsWithToken(event.data.idToken);
+      //list(event.data.idToken);
+      updateAwsCredentialsWithToken(event.data.idToken);
+      log("aws config should be updated...")
+      log(AWS.config.credentials);
     } else {
       //updateAwsCredentials(event.data.idToken)
       const messageReceived = Promise.resolve()
@@ -153,21 +124,23 @@ if (typeof self === 'object') {
   // - to serve authenticated resources directly from S3 transparently for cesium
   self.addEventListener('fetch', function(event) {
     if (event.request.url.includes('/tiles/')){
+      log(`try to fetch this resource with service worker: ${normalizeUrl(event.request.url).substring(1)}`);
       if (self.s3){
         //const url = 'https://s3-eu-central-1.amazonaws.com/ngm-dev-authenticated-resources' + normalizeUrl(event.request.url);
         const s3Key = normalizeUrl(event.request.url).substring(1);
         log('get this s3Key: ' + s3Key);
+        event.respondWith(getObject(s3Key));
         //log(AWS.CognitoIdentityCredentials);
-        var params = {
-          Key: s3Key
-        };
-        self.s3.getObject(params, function(err, data) {
-          if (err) log(err, err.stack); // an error occurred
-          else {
-              log(data);           // successful response
-              event.respondWith(data);
-          }
-        });
+        // var params = {
+        //   Key: s3Key
+        // };
+        // self.s3.getObject(params, function(err, data) {
+        //   if (err) log(err, err.stack); // an error occurred
+        //   else {
+        //       log(data);           // successful response
+        //       event.respondWith(data);
+        //   }
+        // });
       }
     } else {
       event.respondWith(fetch(event.request));
