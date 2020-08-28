0fa7f5957ea2dba93ff9ffef7a72de74244fcdca
Andrea Borghi <andrea.borghi@camptocamp.com>
2020-08-28 11:13:52 +0200

    update terrain timestamp

--- src/viewer.js
+++ src/viewer.js
@@ -93,7 +93,7 @@ export function setupViewer(container) {
   let terrainUrl;
   const ownTerrain = searchParams.get('ownterrain') !== 'false';
   if (ownTerrain) {
-    terrainUrl = 'https://3d.geo.admin.ch/1.0.0/ch.swisstopo.terrain.3d/default/20180601/4326/';
+    terrainUrl = 'https://3d.geo.admin.ch/1.0.0/ch.swisstopo.terrain.3d/default/20200520/4326/';
   } else {
     terrainUrl = IonResource.fromAssetId(1);
   }
