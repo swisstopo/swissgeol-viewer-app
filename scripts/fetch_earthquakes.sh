fd45ba63fd459f73ffb9d04e257eaed1a5d53618
Balthasar Teuscher <balthasar.teuscher@camptocamp.com>
2021-03-08 13:31:21 +0100

    upload to s3://ngmpub-download-bgdi-ch/

--- scripts/fetch_earthquakes.sh
+++ scripts/fetch_earthquakes.sh
@@ -3,17 +3,31 @@ set -e
 set -x
 
 FILENAME="earthquakes.txt"
-TEMPFILE="earthquakes_temp.txt"
 
-LATEST=$(sed '2q;d' $FILENAME)
-STARTTIME="$(cut -d'|' -f2 <<<"$LATEST")"
-NOW=$(date +'%FT%H%M%S')
+OLD_RECORDS=$(curl --fail https://download.swissgeol.ch/earthquakes/$FILENAME)
 
-URL="http://arclink.ethz.ch/fdsnws/event/1/query?starttime=${STARTTIME}&endtime=${NOW}&minmagnitude=1&format=text&nodata=404"
-echo "$URL"
+STARTTIME="$(cut -d'|' -f2 <<<"$(echo "$OLD_RECORDS" | sed '2q;d')")"
+NOW=$(date +'%FT%H:%M:%S')
 
-RECORDS=$(curl --fail $URL)
+NEW_RECORDS=$(curl --fail "http://arclink.ethz.ch/fdsnws/event/1/query?starttime=$STARTTIME&endtime=$NOW&minmagnitude=1&format=text&nodata=404")
 
-echo "$RECORDS" | tee $TEMPFILE
-echo "$(sed 1,2d $FILENAME)" | tee -a $TEMPFILE
-mv $TEMPFILE $FILENAME
+echo "$NEW_RECORDS" | tee $FILENAME
+echo "$(echo "$OLD_RECORDS" | sed '1,2d')" | tee -a $FILENAME
+
+# if no AWS config exists, create it.
+if [[ ! -d ~/.aws ]] ; then
+  mkdir ~/.aws
+fi
+
+set +x # do not output AWS credentials on a public github action!!!!
+cat >> ~/.aws/config << EOF
+[profile ngmdownload]
+aws_access_key_id=${AWS_ACCESS_KEY_ID}
+aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}
+region=eu-west-1
+EOF
+set -x
+
+# cat "$NEW_RECORDS\n$(echo "$OLD_RECORDS" | sed '1,2d')"
+aws s3 --profile ngmdownload --debug cp --cache-control no-cache $FILENAME s3://ngmpub-download-bgdi-ch/earthquakes/
+exit $?
