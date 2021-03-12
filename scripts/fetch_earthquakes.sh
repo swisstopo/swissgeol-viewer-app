aaeff629c69c043dc69e5537346a2d53aa00ec2d
Balthasar Teuscher <balthasar.teuscher@camptocamp.com>
2021-03-12 12:14:29 +0100

    Just fetch the last 90 days

--- scripts/fetch_earthquakes.sh
+++ scripts/fetch_earthquakes.sh
@@ -4,15 +4,10 @@ set -x
 
 FILENAME="earthquakes.txt"
 
-OLD_RECORDS=$(curl --fail https://download.swissgeol.ch/earthquakes/$FILENAME)
+ENDTIME=$(date +'%FT%H:%M:%S')
+STARTTIME=$(date -d '-90 days' +'%FT%H:%M:%S')
 
-STARTTIME="$(cut -d'|' -f2 <<<"$(echo "$OLD_RECORDS" | sed '2q;d')")"
-NOW=$(date +'%FT%H:%M:%S')
-
-NEW_RECORDS=$(curl --fail "http://arclink.ethz.ch/fdsnws/event/1/query?starttime=$STARTTIME&endtime=$NOW&minmagnitude=1&format=text&nodata=404")
-
-echo "$NEW_RECORDS" | tee $FILENAME
-echo "$(echo "$OLD_RECORDS" | sed '1,2d')" | tee -a $FILENAME
+curl --fail "http://arclink.ethz.ch/fdsnws/event/1/query?starttime=$STARTTIME&endtime=$ENDTIME&minmagnitude=1&format=text&nodata=404" > $FILENAME
 
 # if no AWS config exists, create it.
 if [[ ! -d ~/.aws ]] ; then
@@ -28,6 +23,5 @@ region=eu-west-1
 EOF
 set -x
 
-# cat "$NEW_RECORDS\n$(echo "$OLD_RECORDS" | sed '1,2d')"
 aws s3 --profile ngmdownload --debug cp --cache-control no-cache $FILENAME s3://ngmpub-download-bgdi-ch/earthquakes/
 exit $?
