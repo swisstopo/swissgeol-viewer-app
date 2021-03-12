89b145e40e2a81001498f70920bc14168a5f2760
vladyslavtk <v.tkachenko@mobilunity.com>
2021-03-12 11:45:22 +0200

    use test url for cors

--- src/elements/ngm-share-link.js
+++ src/elements/ngm-share-link.js
@@ -38,7 +38,7 @@ class NgmShareLink extends LitElementI18n {
 
   async getShortlink() {
     try {
-      const result = await fetch('https://link.dev.swissgeol.ch/admin_shrink_url', {
+      const result = await fetch('https://94cf3dc1-9819-4573-9556-2b54b4943629.mock.pstmn.io/admin_shrink_url', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json;charset=UTF-8'
