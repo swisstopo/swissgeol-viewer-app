9929dba508f5b42a7c8e01a204c39761d90b5e78
Balthasar Teuscher <balthasar.teuscher@camptocamp.com>
2021-03-23 09:31:48 +0100

    WIP: disclaimer

--- src/layers/ngm-layers-item.js
+++ src/layers/ngm-layers-item.js
@@ -75,8 +75,16 @@ export class LayerTreeItem extends LitElementI18n {
   get buttons() {
     return html`
       <div class="ui icon buttons compact mini">
+      ${this.config.disclaimer ?
+        html`<button class="ui button"
+          data-tooltip=${i18next.t('dtd_disclaimer_hint')}
+          data-position="top left"
+          data-variation="mini"
+          @click=${() => window.open(this.config.disclaimer)}>
+            <i class="question circle icon"></i>
+        </button>` : ''}
       ${this.config.zoomToBbox ?
-      html`<button class="ui button"
+        html`<button class="ui button"
           data-tooltip=${i18next.t('dtd_zoom_to_hint')}
           data-position="top left"
           data-variation="mini"
@@ -120,21 +128,23 @@ export class LayerTreeItem extends LitElementI18n {
 
   render() {
     return html`
+      <div class="ngm-displayed-container buttons">
+        ${this.buttons}
+      </div>
       <div class="ngm-displayed-container">
         <div class="ui checkbox">
           <input class="ngm-layer-checkbox" type="checkbox"
             .checked=${this.config.visible}
             @change=${this.changeVisibility}>
-          <label @click=${this.onLabelClicked}>
+          <label class="ngm-layer-label" @click=${this.onLabelClicked}>
             <i class=${this.config.restricted ? 'lock icon' : ''}></i>${i18next.t(this.label)}
             <div class="ui ${this.loading > 0 ? 'active' : ''} inline mini loader layerloader">
               <span class="small_load_counter">${this.loading}</span>
             </div>
           </label>
         </div>
-        ${this.buttons}
       </div>
-      <div class="ngm-displayed-container" ?hidden=${!this.config.setTransparency}>
+      <div class="ngm-displayed-container transparency" ?hidden=${!this.config.setTransparency}>
         <label>${i18next.t('dtd_transparency_label')} </label>
         <div class="ui grey small slider"></div>
       </div>
