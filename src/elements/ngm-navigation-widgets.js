361c9d76a12f0b8fbb5a0f29013774b8f6e69e3d
Frederic Junod <frederic.junod@camptocamp.com>
2021-09-09 15:06:27 +0200

    Remove cesium-minimap

--- src/elements/ngm-navigation-widgets.js
+++ src/elements/ngm-navigation-widgets.js
@@ -1,7 +1,6 @@
 import {html} from 'lit-element';
 import '@geoblocks/cesium-compass';
 import '@geoblocks/cesium-view-cube';
-import './cesium-minimap.js';
 import './ngm-zoom-buttons.js';
 import './ngm-elevator-buttons.js';
 import './ngm-keyboard-info-popup.js';
@@ -10,21 +9,18 @@ import './ngm-camera-information.js';
 import './slicer/ngm-slicer.js';
 import i18next from 'i18next';
 import {LitElementI18n} from '../i18n.js';
-import {SWITZERLAND_RECTANGLE} from '../constants.js';
 
 class NgmNavigationWidgets extends LitElementI18n {
 
   static get properties() {
     return {
       viewer: {type: Object},
-      minimapExpanded: {type: Boolean},
       slicer: {type: Object}
     };
   }
 
   constructor() {
     super();
-    this.minimapExpanded = true;
     /**
      * @type {import('cesium').Viewer}
      */
@@ -34,19 +30,7 @@ class NgmNavigationWidgets extends LitElementI18n {
   render() {
     if (this.viewer) {
       return html`
-        <cesium-minimap
-          .scene=${this.viewer.scene}
-          extent="[5.910642046, 45.791912227, 10.554524194, 47.804750923]"
-          ?expanded=${this.minimapExpanded}
-          @sizechanged=${evt => this.minimapExpanded = evt.detail.expanded}
-          .mapRectangle="${SWITZERLAND_RECTANGLE}">
-          <i slot="collapse-icon" class="compress arrows alternate icon"></i>
-          <i slot="expand-icon" class="expand arrows alternate icon"></i>
-          <img slot="marker" src="src/images/mapMarker.svg">
-          <img slot="image" src="src/images/overview.svg">
-        </cesium-minimap>
         <ngm-camera-information
-          ?hidden=${!this.minimapExpanded}
           .scene="${this.viewer.scene}" class="item">
         </ngm-camera-information>
         <div class="ngm-navigation-buttons">
