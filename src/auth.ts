c0d8eca1308dc6dd74d9a344c691682d583a3d97
vladyslavtk <v.tkachenko@mobilunity.com>
2021-09-23 19:29:52 +0300

    temporary fix for project start

--- src/auth.ts
+++ src/auth.ts
@@ -4,6 +4,9 @@ import {
   fromCognitoIdentityPool
 } from '@aws-sdk/credential-provider-cognito-identity';
 import {CognitoIdentityClient} from '@aws-sdk/client-cognito-identity';
+// todo we have conflict between build and start
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import auth from './store/auth.ts';
 
 const cognitoState = 'cognito_state';
