b2cb3791b8e9fbc075bcef197aabdd34ef2e9fca
Frederic Junod <frederic.junod@camptocamp.com>
2021-10-18 16:58:45 +0200

    Adapt query tolerance based on the camera distance

--- src/query/SwisstopoIdentify.ts
+++ src/query/SwisstopoIdentify.ts
@@ -2,10 +2,20 @@ import Cartographic from 'cesium/Source/Core/Cartographic';
 import Cartesian3 from 'cesium/Source/Core/Cartesian3';
 import {radiansToLv95} from '../projection';
 
-const getIdentifyUrl = ({geom2056, lang, layers}) =>
-  `https://api3.geo.admin.ch/rest/services/all/MapServer/identify?geometry=${geom2056}&geometryFormat=geojson&geometryType=esriGeometryPoint&mapExtent=0,0,100,100&imageDisplay=100,100,100&lang=${lang}&layers=all:${layers.join()}&limit=1&returnGeometry=true&sr=2056&tolerance=100`;
+const getIdentifyUrl = ({geom2056, lang, layers, tolerance}) =>
+  `https://api3.geo.admin.ch/rest/services/all/MapServer/identify?geometry=${geom2056}&geometryFormat=geojson&geometryType=esriGeometryPoint&mapExtent=0,0,100,100&imageDisplay=100,100,100&lang=${lang}&layers=all:${layers.join()}&limit=1&returnGeometry=true&sr=2056&tolerance=${tolerance}`;
 const getPopupUrl = ({layerBodId, featureId, lang}) =>
   `https://api3.geo.admin.ch/rest/services/api/MapServer/${layerBodId}/${featureId}/htmlPopup?lang=${lang}`;
+
+const getTolerance = (distance: number) => {
+  if (distance > 100000) {
+    return 600;
+  } if (distance < 2500) {
+    return 20;
+  } else {
+    return 200;
+  }
+};
 
 
 export interface IdentifyResult {
@@ -17,14 +27,16 @@ export default class SwisstopoIdentify {
 
   /**
    * @param {Cartesian3} position
+   * @param {number} distance
    * @param {Array<string>} layers
    * @param {string} lang
    * @return {Promise<IdentifyResult | void>} the popup text
    */
-  async identify(position: Cartesian3, layers: Array<string>, lang: string): Promise<IdentifyResult | void> {
+  async identify(position: Cartesian3, distance: number, layers: Array<string>, lang: string): Promise<IdentifyResult | void> {
     const carto = Cartographic.fromCartesian(position);
     const geom2056 = radiansToLv95([carto.longitude, carto.latitude]);
-    const url = getIdentifyUrl({geom2056, lang, layers});
+    const tolerance = getTolerance(distance);
+    const url = getIdentifyUrl({geom2056, lang, layers, tolerance});
     return fetch(url)
       .then(response => response.json())
       .then(data => data.results)
