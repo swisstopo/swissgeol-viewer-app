73ef1a01d9471ff1c3c0b0a38c45000fadfe8e43
Guillaume Beraudo <guillaume.beraudo@gmail.com>
2021-11-02 16:57:36 +0100

    Allow running cypress test with default language to French

--- cypress/integration/sample_spec.ts
+++ cypress/integration/sample_spec.ts
@@ -3,6 +3,7 @@ describe('The Home Page', () => {
     cy.visit('/');
     // eslint-disable-next-line cypress/no-unnecessary-waiting
     cy.wait(15000);
-    cy.get('ngm-tracking-consent').contains('Continue without data acquisition').click();
+    cy.get('ngm-tracking-consent').contains(
+      /Continue without data acquisition|Continuer sans acquisition de données/).click();
   });
 });
