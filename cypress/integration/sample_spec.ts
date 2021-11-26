884ee509206977c23b4568b269c8325a463a209a
vladyslavtk <v.tkachenko@mobilunity.com>
2021-11-26 17:40:16 +0200

    update test

--- cypress/integration/sample_spec.ts
+++ cypress/integration/sample_spec.ts
@@ -1,8 +1,7 @@
 describe('The Home Page', () => {
   it('sucessfully loads', () => {
     cy.visit('/');
-    cy.get('.ngm-determinate-loader > .loader', {timeout: 1000}).not('.determinate');
-    cy.get('.ngm-determinate-loader > .loader', {timeout: 4000}).should('have.class', 'determinate');
+    cy.get('.ngm-main-load-dimmer .ngm-determinate-loader > .loader', {timeout: 4000}).should('have.class', 'determinate');
     cy.get('.ngm-main-load-dimmer').not('.active', {timeout: 180000});
     cy.get('ngm-tracking-consent').contains(
       /Continue without data acquisition|Continuer sans acquisition de données/).click();
