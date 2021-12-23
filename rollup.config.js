7884acf4fbac3362e8885aaa093ff951a56952ad
Frederic Junod <frederic.junod@camptocamp.com>
2021-12-23 09:56:02 +0100

    Remove webcomponentsjs polyfill

--- rollup.config.js
+++ rollup.config.js
@@ -89,7 +89,6 @@ const config = {
         {src: 'src/images', dest: 'dist/'},
         {src: 'node_modules/@fontsource/inter/files/*', dest: 'dist/fonts/'},
         {src: 'node_modules/fomantic-ui-css/themes/default/assets/fonts/*', dest: 'dist/fonts/'},
-        {src: 'node_modules/@webcomponents/webcomponentsjs/*', dest: 'dist/webcomponentsjs/'},
         {src: 'manuals/dist/*', dest: 'dist/manuals/'},
         {src: 'manuals/images/', dest: 'dist/manuals/'},
         {src: 'manuals/style.css', dest: 'dist/manuals/'},
