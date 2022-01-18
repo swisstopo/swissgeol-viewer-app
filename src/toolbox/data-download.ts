3e3e05cba7639e13f40ecb887dfb604f27d7762f
Balthasar Teuscher <balthasar.teuscher@camptocamp.com>
2022-01-18 13:08:10 +0100

    Display data download options

--- src/toolbox/data-download.ts
+++ src/toolbox/data-download.ts
@@ -1,20 +1,33 @@
 import {CustomDataSource} from 'cesium';
 import {html} from 'lit';
-import {customElement, property} from 'lit/decorators.js';
+import {customElement, property, state} from 'lit/decorators.js';
 import {LitElementI18n} from '../i18n';
+import {NgmGeometry} from './interfaces';
 
 
 @customElement('data-download')
 export class DataDownload extends LitElementI18n {
   @property({type: Object}) geometriesDataSource: CustomDataSource | undefined;
-  sliceGeomId: unknown;
+  @state() selectedId: string = '';
 
+  downloadOptionsTemplate(options) {
+
+    return html`
+      <div class="data-download-options" ?hidden=${options.geom.id !== this.selectedId}>
+          Data download options
+      </div>
+    `
+  }
 
   render() {
     return html`
       <ngm-geometries-list
-        .selectedId=${this.sliceGeomId}
-        .disabledTypes=${['point', 'polygon', 'line']}>
+        .selectedId=${this.selectedId}
+        .disabledTypes=${['point', 'polygon', 'line']}
+        .optionsTemplate=${(geom) => this.downloadOptionsTemplate({geom})}
+        @geomclick=${(evt: CustomEvent<NgmGeometry>) => {
+          this.selectedId = this.selectedId !== evt.detail.id ? evt.detail.id! : ''
+          }}>
       </ngm-geometries-list>
     `;
   }
