0a13cd212df2b41e908fee688a999237734adb51
vladyslavtk <v.tkachenko@mobilunity.com>
2022-01-26 14:21:48 +0200

    open just added rectangle

--- src/toolbox/data-download.ts
+++ src/toolbox/data-download.ts
@@ -38,10 +38,10 @@ export class DataDownload extends LitElementI18n {
     // Fetch data
     const data: any[] = [];
     const specs = [{
-        layer: config.layer!,
-        url: config.downloadDataPath!,
-        type: config.downloadDataType!
-      }];
+      layer: config.layer!,
+      url: config.downloadDataPath!,
+      type: config.downloadDataType!
+    }];
     for await (const d of createDataGenerator(specs, bbox)) data.push(d);
     if (data.length === 0) {
       showSnackbarInfo(i18next.t('tbx_no_data_to_download_warning'));
@@ -53,33 +53,45 @@ export class DataDownload extends LitElementI18n {
     saveAs(blob, `${config.layer!}_${geom.name}.zip`.replace(/\s/g, '_'));
   }
 
+
+  onGeomClick(geomId) {
+    this.selectedGeometryId = this.selectedGeometryId !== geomId ? geomId : '';
+  }
+
+  onGeometryAdded(newGeometries: NgmGeometry[]) {
+    if (this.hidden) return;
+    for (const geom of newGeometries) {
+      if (geom.type === 'rectangle') {
+        this.onGeomClick(geom.id);
+      }
+    }
+  }
+
   downloadOptionsTemplate(geom: NgmGeometry) {
     if (geom.id !== this.selectedGeometryId) {
       return html``;
     }
 
     const activeLayers = this.activeLayersForDownload();
-    const content = activeLayers.length ? html`
-      ${activeLayers.map((config: Config) => {
-      return html`
-        <div class="data-download-item">
-          <div class="ngm-file-download-icon" @click=${() => {
-            if (config.downloadDataType && config.downloadDataPath) {
-              this.downloadData(config, geom);
-            } else {
-              window.open(config.downloadUrl);
-            }
-          }}></div>
-          <label>${i18next.t(config.label)}</label>
+    const content = activeLayers.length ?
+      activeLayers.map((config: Config) =>
+        html`
+          <div class="data-download-item">
+            <div class="ngm-file-download-icon" @click=${() => {
+              if (config.downloadDataType && config.downloadDataPath) {
+                this.downloadData(config, geom);
+              } else {
+                window.open(config.downloadUrl);
+              }
+            }}></div>
+            <label>${i18next.t(config.label)}</label>
+          </div>
+        `) : html`
+        <div class="data-download-hint">
+          ${i18next.t('tbx_data_download_hint')}
+          <div class="ngm-info-icon"></div>
         </div>
-        `;
-    })}
-    ` : html`
-      <div class="data-download-hint">
-        ${i18next.t('tbx_data_download_hint')}
-        <div class="ngm-info-icon"></div>
-      </div>
-    `;
+      `;
 
     return html`
       <div class="data-download-options">
@@ -96,9 +108,8 @@ export class DataDownload extends LitElementI18n {
         .selectedId=${this.selectedGeometryId}
         .disabledTypes=${['point', 'polygon', 'line']}
         .optionsTemplate=${(geom: NgmGeometry) => this.downloadOptionsTemplate(geom)}
-        @geomclick=${(evt: CustomEvent<NgmGeometry>) => {
-        this.selectedGeometryId = this.selectedGeometryId !== evt.detail.id ? evt.detail.id! : '';
-      }}
+        @geomclick=${(evt: CustomEvent<NgmGeometry>) => this.onGeomClick(evt.detail.id)}
+        @geometriesadded=${evt => this.onGeometryAdded(evt.detail.newGeometries)}
       ></ngm-geometries-list>
     `;
   }
