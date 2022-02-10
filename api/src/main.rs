4cfe7c9998b7548d40d57579c2be8af789992c63
Guillaume Beraudo <guillaume.beraudo@gmail.com>
2022-02-10 14:11:11 +0100

    Check both ui and api in CI

--- api/src/main.rs
+++ api/src/main.rs
@@ -3,11 +3,13 @@ use axum::{routing::get, Router};
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/api", get(|| async { "H sdfsdfello, Worsfsdfld!" }));
+    println!("Starting server...");
+    let app = Router::new().route("/api/health_check", get(|| async { "Healthy" }));
 
     // run it with hyper on localhost:3000
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
+    println!("Ending server...");
 }
