f7f2af09238d0ab6792998ad49bb4a0919eff727
Guillaume Beraudo <guillaume.beraudo@gmail.com>
2022-09-16 11:13:34 +0200

    Increase timeout

--- ui/cypress/support/index.ts
+++ ui/cypress/support/index.ts
@@ -1,6 +1,6 @@
 Cypress.Commands.add('loadPage', (doNotHideCookie = false, reload = false) => {
   reload ? cy.reload() : cy.visit('/');
-  cy.get('.ngm-main-load-dimmer .ngm-determinate-loader > .loader', {timeout: 4000}).should('have.class', 'determinate');
+  cy.get('.ngm-main-load-dimmer .ngm-determinate-loader > .loader', {timeout: 7000}).should('have.class', 'determinate');
   cy.get('.ngm-main-load-dimmer').not('.active', {timeout: 60000});
   if (!doNotHideCookie)
     cy.get('ngm-tracking-consent').contains(
