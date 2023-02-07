62c65a91ef9407d7e45c0e7ec01f029fef64a26b
Frederic Junod <frederic.junod@camptocamp.com>
2023-02-07 16:08:19 +0100

    Reset ngm-voxel-filter on close

--- ui/src/elements/ngm-voxel-filter.ts
+++ ui/src/elements/ngm-voxel-filter.ts
@@ -1,6 +1,6 @@
 import i18next from 'i18next';
 import {html, nothing} from 'lit';
-import {customElement, property, queryAll} from 'lit/decorators.js';
+import {customElement, property, query, queryAll} from 'lit/decorators.js';
 import {LitElementI18n} from '../i18n';
 import draggable from './draggable';
 import {dragArea} from './helperElements';
@@ -14,7 +14,11 @@ import {setBit} from '../utils';
 export class NgmVoxelFilter extends LitElementI18n {
   @property({type: Object}) config!: Config;
   @property({type: Object}) viewer!: Viewer;
+
   @queryAll('.lithology-checkbox input[type="checkbox"]') lithologyCheckbox!: NodeListOf<HTMLInputElement>;
+  @query('.min-conductivity') minConductivityInput!: HTMLInputElement;
+  @query('.max-conductivity') maxConductivityInput!: HTMLInputElement;
+
   private minConductivity = NaN;
   private maxConductivity = NaN;
 
@@ -28,6 +32,7 @@ export class NgmVoxelFilter extends LitElementI18n {
   willUpdate() {
     this.minConductivityValue = this.minConductivity = this.config.voxelFilter!.conductivityRange[0];
     this.maxConductivityValue = this.maxConductivity = this.config.voxelFilter!.conductivityRange[1];
+
     this.hidden = false;
   }
 
@@ -38,21 +43,21 @@ export class NgmVoxelFilter extends LitElementI18n {
         <div class="ngm-close-icon" @click=${() => this.close()}></div>
       </div>
       <div class="content-container">
-        <div class="ui form">
+        <form class="ui form">
           <div class="filter-label">${i18next.t('vox_filter_hydraulic_conductivity')}</div>
           <div class="two fields">
             <div class="field">
               <label>${i18next.t('vox_filter_min')}</label>
-              <input type="number" step="0.01" .value="${this.minConductivity}" min="${this.minConductivityValue}" @input="${evt => this.minConductivity = evt.target.value}"/>
+              <input class="min-conductivity" type="number" step="0.01" value="${this.minConductivity}" min="${this.minConductivityValue}" max="${this.maxConductivity}" @input="${evt => this.minConductivityChanged(evt)}"/>
             </div>
             <div class="field">
               <label>${i18next.t('vox_filter_max')}</label>
-              <input type="number" step="0.01" .value="${this.maxConductivity}" max="${this.maxConductivityValue}" @input="${evt => this.maxConductivity = evt.target.value}"/>
+              <input class="max-conductivity" type="number" step="0.01" value="${this.maxConductivity}" min="${this.minConductivity}" max="${this.maxConductivityValue}" @input="${evt => this.maxConductivityChanged(evt)}"/>
             </div>
           </div>
-        </div>
+        </form>
         ${this.config.voxelFilter?.lithology ? html`
-        <div class="ui form">
+        <form class="ui form">
           <div class="inline fields">
             <div class="field">
               <div class="ui radio checkbox">
@@ -73,13 +78,13 @@ export class NgmVoxelFilter extends LitElementI18n {
               </div>
             </div>
           </div>
-        </div>
-        <div class="lithology-checkbox">
+        </form>
+        <form class="lithology-checkbox">
           <div class="filter-label">${i18next.t('vox_filter_lithology')}</div>
           ${repeat(this.config.voxelFilter.lithology, (lithology: any) =>
             html`<label><input type="checkbox" value="${lithology.index}" checked> ${lithology.label}</label>`
           )}
-        </div>
+        </form>
         ` : nothing}
         <div>
           <button class="ui button" @click="${() => this.applyFilter()}">
@@ -91,9 +96,22 @@ export class NgmVoxelFilter extends LitElementI18n {
     `;
   }
 
+  minConductivityChanged(evt: any) {
+    this.minConductivity = parseFloat(evt.target.value);
+    this.maxConductivityInput.min = this.minConductivity.toString();
+  }
+
+
+  maxConductivityChanged(evt) {
+    this.maxConductivity = parseFloat(evt.target.value);
+    this.minConductivityInput.max = this.maxConductivity.toString();
+  }
+
   close() {
     this.hidden = true;
-    this.resetFilter();
+    this.resetShader();
+    this.resetForm();
+
     this.config = undefined;
   }
 
@@ -109,15 +127,20 @@ export class NgmVoxelFilter extends LitElementI18n {
       }
     });
     shader.setUniform('u_filter_lithology_exclude', lithologyExclude);
-    const operator = this.querySelector<HTMLInputElement>('input[name="operator"]:checked');
-    if (operator) {
-      shader.setUniform('u_filter_operator', operator.value);
-    }
+    const operator = this.querySelector<HTMLInputElement>('input[name="operator"]:checked')!;
+    shader.setUniform('u_filter_operator', operator.value);
+
+    console.log({
+      u_filter_conductivity_min: this.minConductivity,
+      u_filter_conductivity_max: this.maxConductivity,
+      u_filter_operator: operator.value,
+      u_filter_lithology_exclude: lithologyExclude,
+    });
 
     this.viewer.scene.requestRender();
   }
 
-  resetFilter() {
+  resetShader() {
     const shader = getVoxelShader(this.config);
     shader.setUniform('u_filter_conductivity_min', this.minConductivityValue);
     shader.setUniform('u_filter_conductivity_max', this.maxConductivityValue);
@@ -126,6 +149,9 @@ export class NgmVoxelFilter extends LitElementI18n {
     this.viewer.scene.requestRender();
   }
 
+  resetForm() {
+    this.querySelectorAll<HTMLFormElement>('.content-container form').forEach(form => form.reset());
+  }
 
   firstUpdated() {
     draggable(this, {
