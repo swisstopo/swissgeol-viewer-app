4989d023047812e6042f5fa8e0e3bf6ad79a1acf
Elisabeth Leu <elisabeth.leu@camptocamp.com>
2023-07-10 11:06:45 +0200

    remove double &

--- scripts/fetch_earthquakes.sh
+++ scripts/fetch_earthquakes.sh
@@ -9,7 +9,7 @@ ENDTIME=$(date +'%FT%H:%M:%S')
 FILENAME="earthquakes_last_90d.txt"
 STARTTIME=$(date -d '-90 days' +'%FT%H:%M:%S')
 
-curl --fail "http://arclink.ethz.ch/fdsnws/event/1/query?starttime=$STARTTIME&endtime=$ENDTIME&minmagnitude=1&format=text&nodata=404&&EventType=earthquake" > $FILENAME
+curl --fail "http://arclink.ethz.ch/fdsnws/event/1/query?starttime=$STARTTIME&endtime=$ENDTIME&minmagnitude=1&format=text&nodata=404&EventType=earthquake" > $FILENAME
 
 aws s3 --debug cp --cache-control no-cache $FILENAME s3://ngmpub-download-bgdi-ch/earthquakes/
 
@@ -17,6 +17,6 @@ aws s3 --debug cp --cache-control no-cache $FILENAME s3://ngmpub-download-bgdi-c
 FILENAME="earthquakes_magnitude_gt_3.txt"
 STARTTIME="1979-01-01T00:00:00"
 
-curl --fail "http://arclink.ethz.ch/fdsnws/event/1/query?starttime=$STARTTIME&endtime=$ENDTIME&minmagnitude=3&format=text&nodata=404&&EventType=earthquake" > $FILENAME
+curl --fail "http://arclink.ethz.ch/fdsnws/event/1/query?starttime=$STARTTIME&endtime=$ENDTIME&minmagnitude=3&format=text&nodata=404&EventType=earthquake" > $FILENAME
 
 aws s3 --debug cp --cache-control no-cache $FILENAME s3://ngmpub-download-bgdi-ch/earthquakes/
