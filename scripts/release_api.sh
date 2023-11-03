9ff5c06134b0b4134b257bccd8fa418c1053f39b
vladyslavtk <vladyslav.tkachenko@camptocamp.com>
2023-11-03 16:15:09 +0200

    Update API build process for macos

--- scripts/release_api.sh
+++ scripts/release_api.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -e
+
+os=$1 # mac for macos and empty for linux
+
 cd api
 
 IMAGE_NAME="camptocamp/swissgeol_api"
@@ -10,5 +13,10 @@ then
 fi
 
 
-docker build --pull -t $IMAGE_NAME:$VERSION .
+if [[ "$os" = "mac" ]]
+then
+  docker build --platform linux/amd64 --pull -t $IMAGE_NAME:$VERSION . --build-arg mac=true --no-cache
+else
+  docker build --pull -t $IMAGE_NAME:$VERSION .
+fi
 docker push $IMAGE_NAME:$VERSION
