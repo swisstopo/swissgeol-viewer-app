e185a7270e5add389ddcd39db736e2bc03d8fe21
Pedro Soares <pedro.soares@camptocamp.com>
2023-11-22 16:42:11 +0100

    change exaggeration param to zExaggeration

--- ui/src/viewer.ts
+++ ui/src/viewer.ts
@@ -98,8 +98,11 @@ export interface BaseLayerConfig {
 export async function setupViewer(container: Element, rethrowRenderErrors: boolean) {
 
     const searchParams = new URLSearchParams(location.search);
+    console.log(location);
 
-    const terrainExaggeration = parseFloat(searchParams.get('terrainExaggeration') || '1');
+    let zExaggeration = parseFloat(searchParams.get('zExaggeration') || '1');
+    zExaggeration = (zExaggeration >= 1 && zExaggeration <= 100) ? zExaggeration : 1;
+
     if (searchParams.get('noLimit') === 'false') {
         noLimit = false;
     }
@@ -160,7 +163,7 @@ export async function setupViewer(container: Element, rethrowRenderErrors: boole
     enableCenterOfRotate(viewer);
 
     const globe = scene.globe;
-    globe.terrainExaggeration = terrainExaggeration;
+    globe.terrainExaggeration = zExaggeration;
 
     if (searchParams.get('swissrectangle') !== 'false') {
         const rectangle = Rectangle.fromDegrees(
