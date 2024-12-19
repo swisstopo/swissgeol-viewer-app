e9ce5264114604e9d717923be6f357ad83cf6af6
till_schuetze <till.schuetze@ebp.ch>
2024-12-19 14:59:28 +0100

    Start styling the Tab header

--- ui/src/components/data-display/tab-display.ts
+++ ui/src/components/data-display/tab-display.ts
@@ -1,5 +1,5 @@
 import {customElement, property, query, state} from "lit/decorators.js";
-import {html} from "lit";
+import {css, html} from "lit";
 import {LitElementI18n} from "../../i18n";
 import {DEFAULT_LAYER_OPACITY, LayerConfig} from "../../layertree";
 import '../layers/layers-catalog';
@@ -9,6 +9,7 @@ import MainStore from "../../store/main";
 import {LayerEvent} from "../layers/layers-display";
 import {Subscription} from "rxjs";
 import './tabs/exaggeration-slider'
+import {classMap} from "lit/directives/class-map.js";
 
 @customElement('ngm-tab-display')
 export class DataUpload extends  LitElementI18n {
@@ -25,6 +26,10 @@ export class DataUpload extends  LitElementI18n {
   private accessor viewer: Viewer | null = null
 
   private readonly subscription = new Subscription();
+
+  //TODO: where is this from exactly?
+  @query('.ngm-side-bar-panel > .ngm-toast-placeholder')
+  accessor toastPlaceholder;
   constructor() {
     super();
     this.handleKmlUpload = this.handleKmlUpload.bind(this);
@@ -37,14 +42,60 @@ export class DataUpload extends  LitElementI18n {
     }));
   }
 
-  //TODO: where is this from exactly?
-  @query('.ngm-side-bar-panel > .ngm-toast-placeholder')
-  accessor toastPlaceholder;
+  static readonly styles = css`
+    .container {
+      padding: 16px
+    }
+    .tabs {
+      display: flex;
+      justify-content: space-evenly;
+      align-items: center;
+      background-color: white;
+      height: 52px;
+      border-radius: 4px;
+      padding: 0 6px;
+    }
+
+    .tabs > button {
+      background-color: transparent;
+      border: none;
+      padding: 8px;
+      cursor: pointer;
+      border-radius: 4px;
+      height: 40px;
+      flex: 1;
+      color: var(--color-main);
+      font-family: var(--font);
+      font-size: 14px;
+      font-weight: 500;
+      line-height: 20px;
+    }
+
+    .tabs > button.active {
+      background-color: var(--color-rest-active);
+      color: var(--color-medium-emphasis);
+    }
+
+    .tabs > div {
+      border: 1px solid #E0E1E4;
+      height: 18px;
+    }
+
+    .tabs > div.hide-border {
+      border-color: transparent;
+    }
+
+    `;
+
   readonly render =() => html`
-    <div>
-      <button @click="${() => this.activeTab = Tab.Catalog}">Katalog</button>
-      <button @click="${() => this.activeTab = Tab.Upload}">Upload</button>
-      <button @click="${() => this.activeTab = Tab.Exaggeration}">Einstellungen</button>
+    <div class="container">
+      <div class="tabs">
+        <button @click="${() => this.activeTab = Tab.Catalog}" class="${classMap({'active': this.activeTab == Tab.Catalog})}">Katalog</button>
+        <div class="${classMap({'hide-border': this.activeTab != Tab.Exaggeration})}"></div>
+        <button @click="${() => this.activeTab = Tab.Upload}" class="${classMap({'active': this.activeTab == Tab.Upload})} center" >Upload</button>
+        <div class="${classMap({'hide-border': this.activeTab != Tab.Catalog})}"></div>
+        <button @click="${() => this.activeTab = Tab.Exaggeration}" class="${classMap({'active': this.activeTab == Tab.Exaggeration})}">Einstellungen</button>
+      </div>
       <div ?hidden="${this.activeTab !== Tab.Catalog}">
         ${this.renderCatalog()}
       </div>
@@ -62,11 +113,11 @@ export class DataUpload extends  LitElementI18n {
 
   private readonly renderCatalog = () => html`
     <ngm-layers-catalog
-      class="ui accordion ngm-panel-content"
       .layers=${this.layers}
     ></ngm-layers-catalog>
   `;
 
+  //Maybe move the methods to the ngm-data-upload component
   private async handleKmlUpload(file: File, clampToGround: boolean): Promise<void> {
     if (this.viewer == null) {
       return;
@@ -98,7 +149,6 @@ export class DataUpload extends  LitElementI18n {
     await this.viewer.zoomTo(dataSource);
     this.requestUpdate();
   }
-
   private clickLayer(layer: LayerConfig): void {
     this.dispatchEvent(new CustomEvent('layer-click', {
       bubbles: true,
